5a480d83c5e92b69f3fb1d4e219ee3017247142d
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2024-03-25 23:01:06 +0100

    modif nom

--- js/views/pages/PersonnageShow.js
+++ js/views/pages/PersonnageShow.js
@@ -4,12 +4,10 @@ import PersonnagesProvider from "../../services/PersonnagesProvider.js";
 export default class PersonnageShow {
     async render () {
         let request = Utils.parseRequestURL()
-        let post = await PersonnagesProvider.getPersonnage(request.nom)
-        
+        let personnage = await PersonnagesProvider.getPersonnage(request.nom)        
         return /*html*/`
             <section class="section">
-                <h2> ${ request }</h2>
-                <h1> ${ post} </h1>
+                <h2> ${ request.nom }</h2>
             </section>
             <p><a href="/">back to home</a></p>
         `
