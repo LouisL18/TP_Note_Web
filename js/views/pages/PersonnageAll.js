043d0c5a3752aa819d85cc70eb82f73b2b6c051c
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2024-03-27 23:24:25 +0100

    test recherche

--- js/views/pages/PersonnageAll.js
+++ js/views/pages/PersonnageAll.js
@@ -1,9 +1,6 @@
 import PersonnagesProvider from "../../services/PersonnagesProvider.js";
 
-// Définissez la fonction changeImage en dehors de la classe
-function changeImage(element) {
-    element.src = "../../../icon/he.png";
-}
+
 
 export default class PersonnageAll {
     constructor() {
@@ -17,36 +14,34 @@ export default class PersonnageAll {
         let favoris = this.favoris;
         // Construire la vue HTML avec les détails des personnages
         let view = `
-            <h2>Tous les Personnages</h2>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            ${personnages.map(personnage => `
-                <div class="col">
-                <div class="card shadow-sm">
-                    <img class="ak-entitylook" alt="" width="250" height="250" src="${personnage.image}">
-                    <div class="card-body">
-                    <p class="card-text">${personnage.nom ? personnage.nom.slice(0,100) : ''}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                        <a href="#/personnages/${personnage.id}" class="btn btn-sm btn-outline-secondary">Voir ${personnage.nom}</a>
-                        ${
-                            this.favoris && this.favoris.includes(personnage.id) ?
-                            `<img src="../../../icon/heart2.png" class="favoris" alt="favoris" width="30" height="30" data-id="${personnage.id}" onclick="console.log('${personnage.id}');">` :
 
-                            `<img src="../../../icon/heart.png" class="favoris" alt="favoris" width="30" height="30" data-id="${personnage.id}" onclick="${this.favoris ? `this.favoris.push('${personnage.id}');` : `this.favoris = ['${personnage.id}'];`} console.log('${personnage.id}');">`
-                        }
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            `).join('\n')}
+            <h2>Tous les personnages</h2>
+            <div class="search">
+                <input type="text" id="searchBar" placeholder="Recherche par nom" oninput="searchByName()">
             </div>
+            <ul class="liste">
+                ${personnages.map(personnage => `
+                    <li>
+                        <div class="card">
+                            ${
+                                this.favoris && this.favoris.includes(personnage.id) ?
+                                `<img src="../../../icon/heart2.png" width="30" height="30" class="favoris" id="${personnage.id}" alt="favoris">` :
+                                `<img src="../../../icon/heart.png" width="30" height="30" class="favoris" id="${personnage.id}" alt="favoris">`
+                            }
+                            <div class="image-container">
+                                <img src="${personnage.image}" alt="${personnage.nom}">
+                            </div>
+                            <h3>${personnage.nom ? personnage.nom.slice(0, 100) : ''}</h3>
+                            <a href="#/personnages/${personnage.id}" class="btn btn-sm btn-outline-secondary">Voir ${personnage.nom}</a>
+                        </div>
+                    </li>
+                `).join('\n')}
+            </ul>
             <div id="favoris-section">
-            <!-- Cette section affichera les favoris -->
+                <!-- Cette section affichera les favoris -->
             </div>
         `;
 
         return view;
     }
-
 }
